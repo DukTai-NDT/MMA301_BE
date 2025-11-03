0cade0a88d53ba58b98e10edadb5db209a4ee253
Tran Duc Hai <haiductran712@gmail.com>
2025-11-04 01:43:32 +0700

    fix merge ducnx

--- src/controllers/vnpayController.js
+++ src/controllers/vnpayController.js
@@ -183,7 +183,7 @@ exports.vnpayReturn = async (req, res) => {
     }
 
     //  FE base URL: địa chỉ Expo Go
-    const FE_BASE = "exp://192.168.1.104:8081";
+    const FE_BASE = "exp://192.168.5.102:8081";
 
     const title =
       code === "00" ? "✅ Thanh toán thành công!" : "❌ Thanh toán thất bại!";
