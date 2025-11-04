4d2585bb5103f6d6f6f0a36d6b7615009e970aa6
Tran Duc Hai <haiductran712@gmail.com>
2025-11-04 10:10:02 +0700

    123

--- config/vnpay.js
+++ config/vnpay.js
@@ -3,6 +3,6 @@ module.exports = {
   vnp_HashSecret: "OYJ7ZN87KWJJ3J0HVZSFJSBSDOUJAOSF",
   vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
   vnp_Api: "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
-  vnp_ReturnUrl: "http://192.168.1.100:9999/api/vnpay/return"
+  vnp_ReturnUrl: "http://192.168.5.102:9999/api/vnpay/return",
 };
 // server.js
